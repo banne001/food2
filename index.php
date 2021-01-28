75d794f85b3ed2d3f58d74141e659e9d50adb269
Blezyl Santos <blezylsantos@yahoo.com>
2021-01-28 15:44:52 -0500

    Added All order pages and summary

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+// Create a Session
+session_start();
 // require the autoload file
 require_once('vendor/autoload.php');
 // create an instance of the Base class
@@ -24,15 +26,29 @@ $f3->route('GET /order', function() {
 });
 
 //Define an order2 route
-$f3->route('GET /order2', function() {
-
-    echo "Order 2 route";
+$f3->route('POST /order2', function() {
+    // Getting Data from order 1
+    var_dump($_POST);
+    if(isset($_POST['food'])){
+        $_SESSION['food'] = $_POST['food'];
+    }
+    if(isset($_POST['meal'])){
+        $_SESSION['meal'] = $_POST['meal'];
+    }
+    $view = new Template();
+    echo $view->render("views/order2.html");
 });
 
 //Define a summary route
-$f3->route('GET /summary', function() {
+$f3->route('POST /summary', function() {
+    var_dump($_POST);
+    var_dump($_SESSION);
 
-    echo "Summary route";
+    if(isset($_POST['conds'])){
+        $_SESSION['conds'] = implode(",", $_POST['conds']) ;
+    }
+    $view = new Template();
+    echo $view->render("views/summary.html");
 });
 
 // run fat free
